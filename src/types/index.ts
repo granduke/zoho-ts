152734c8a8ed35f174a4279a47f4b8c0f7b8f96b
SurferLee <leebennett@stream-surfer.com>
2022-09-07 01:28:49 -0400

    Export CreateLineItems

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,7 @@ export type {
     ContactPersonFromContactGet,
 } from "./contactPerson";
 export type { Document } from "./document";
-export type { LineItem } from "./lineItem";
+export type { LineItem, CreateLineItems } from "./lineItem";
 export type { Package } from "./package";
 export type { Invoice } from "./invoice";
 export type { Tax } from "./tax";
